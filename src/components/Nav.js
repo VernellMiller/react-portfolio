b971db8353da800e1206a7268882afbf7228e862
vernellmiller <nezeey@gmail.com>
2021-12-10 16:11:35 -0500

    refactors code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,15 +4,25 @@ import { Link } from "react-router-dom";
 const Nav = (props) => {
     return (
         <div className="nav">
-
-            <Link to="/">
-                <div>Vernell Miller</div>
-            </Link>
-
-            <Link to="/resume">
-                <div>Resume</div>
-            </Link>
-
+            <nav class="navbar navbar-light bg-light">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <Link to="/" class="text-decoration-none">
+                            <span class="navbar-brand mb-0 h1">VERNELL</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio" class="text-decoration-none">
+                            <div>Portfolio</div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/resume" class="text-decoration-none">
+                            <div>Resume</div>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 };
