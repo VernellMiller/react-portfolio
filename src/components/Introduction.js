cf863438b591033b5e0bc1049eae33f7172fb2a4
vernellmiller <nezeey@gmail.com>
2021-12-10 16:11:15 -0500

    wip: adds introduction

---
+++ src/components/Introduction.js
@@ -0,0 +1,25 @@
+import Button from "react-bootstrap/esm/Button";
+
+const Introduction = (props) => {
+    return(
+        <div>
+
+            <p class="text-center">- introducing</p>
+            <h3>Hello</h3>
+            <h1>I'm Vernell</h1>
+            <h1>Miller</h1>
+            <p class="text-start">An Atlanta-based Full Stack Engineer | 
+            2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>“Individually, we are one drop. Together, we are an ocean.”
+            </p>
+            <p class="text-start">I have a passion for exploring new learning opportunities <br></br>and applying the new knowledge in exciting and creative ways.
+            </p>
+
+            <Button>Contact Me</Button>
+
+
+        </div>
+        
+    );
+};
+
+export default Introduction
