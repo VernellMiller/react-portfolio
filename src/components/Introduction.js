a664eda7dc28a60042f2ce5f517ea0689742c7ad
vernellmiller <nezeey@gmail.com>
2021-12-11 18:20:41 -0500

    wip: refactors code and adds id's

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,24 +1,31 @@
 import Button from "react-bootstrap/esm/Button";
+import { Container } from "react-bootstrap";
 
 const Introduction = (props) => {
     return(
-        <div class="text-start" id="intro">
+        <>
+        <Container>
 
-            <p class="text-start">- introducing</p>
-            <h3 class="text-start">Hello</h3>
-            <h1 class="text-start">I'm Vernell</h1>
-            <h1 class="text-start">Miller</h1>
-            <p class="text-start">An Atlanta-based Full Stack Engineer | 
-            2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>“Individually, we are one drop. Together, we are an ocean.”
-            </p>
-            <p class="text-start">I have a passion for exploring new learning opportunities <br></br>and applying the new knowledge in exciting and creative ways.
-            </p>
+            <div class="text-start" id="intro">
+                <p>- introducing</p>
+                <h3 id="heading">Hello</h3>
+                <h1 id="heading">I'm Vernell</h1>
+                <h1 id="heading">Miller</h1>
+                <p>An Atlanta-based Full Stack Engineer | 
+                2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>“Individually, we are one drop. Together, we are an ocean.”
+                </p>
+                <p>I have a passion for exploring new learning opportunities <br></br>and applying the new knowledge in exciting and creative ways.
+                </p>
 
-            <Button id="contact">Contact Me</Button>
+                <Button id="contact">Contact Me</Button>
+            </div>
 
-
-        </div>
-        
+            <div>
+                <img class="img-fluid" id="mainIMG" src="/me.png" alt="Me" width="900"/>
+                
+            </div>
+        </Container>
+        </> 
     );
 };
 
