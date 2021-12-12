478419e581d3c8cf2c87b50ab9a19b8f6e5f98bc
vernellmiller <nezeey@gmail.com>
2021-12-12 13:09:14 -0500

    wip: adds classes and styling

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,30 +1,30 @@
 import Button from "react-bootstrap/esm/Button";
-import { Container } from "react-bootstrap";
 
 const Introduction = (props) => {
     return(
         <>
-        <Container>
+            <div class="container">
+                <div class="row gap100">
+                    <div class="col-md-6 text-start" id="intro">
+                        <p>- introducing</p>
+                        <h3 id="heading">Hello</h3>
+                        <h1 id="heading">I'm Vernell</h1>
+                        <h1 id="heading" class="fs-1">Miller</h1>
+                        // I might want to try Monospace!!!!
+                        <p>An Atlanta-based Full Stack Engineer | 
+                        2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>“Individually, we are one drop. Together, we are an ocean.”
+                        </p>
+                        <p>I have a passion for exploring new learning opportunities <br></br>and applying the new knowledge in exciting and creative ways.
+                        </p>
 
-            <div class="text-start" id="intro">
-                <p>- introducing</p>
-                <h3 id="heading">Hello</h3>
-                <h1 id="heading">I'm Vernell</h1>
-                <h1 id="heading">Miller</h1>
-                <p>An Atlanta-based Full Stack Engineer | 
-                2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>“Individually, we are one drop. Together, we are an ocean.”
-                </p>
-                <p>I have a passion for exploring new learning opportunities <br></br>and applying the new knowledge in exciting and creative ways.
-                </p>
+                        <Button id="contact">Contact Me</Button>
+                    </div>
 
-                <Button id="contact">Contact Me</Button>
+                    <div class="col-md-6">
+                        <img class="img-fluid" id="mainIMG" src="/me.png" alt="Me" width="900"/>
+                    </div>
+                </div>
             </div>
-
-            <div>
-                <img class="img-fluid" id="mainIMG" src="/me.png" alt="Me" width="900"/>
-                
-            </div>
-        </Container>
         </> 
     );
 };
