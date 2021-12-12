cae68e1a02ff816a09b7ea0dd291654249b0d765
vernellmiller <nezeey@gmail.com>
2021-12-12 17:46:13 -0500

    creates skills and folio components

---
+++ src/components/Folio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Project1 from './Project1'
+import Project2 from './Project2'
+
+const Folio = (props) => {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <Project1 />
+            <Project2 />
+        </div>
+    )
+}
+
+export default Folio
---
+++ src/components/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = (props) => {
+    return (
+        <div class="container bg-light">
+            <p>- My Skills</p>
+            <h1>Why Hire Me For your Next Project?</h1>
+            <p>explanation of my qualifications</p>
+        </div>
+    )
+}
+
+export default Skills;
