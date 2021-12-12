028b424b5e4d52ffa26a30dab0a51692ff3c3556
vernellmiller <nezeey@gmail.com>
2021-12-12 18:50:36 -0500

    adds 4-8 project components

--- src/components/Folio.js
+++ src/components/Folio.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Project1 from './Project1'
 import Project2 from './Project2'
+import Project3 from './Project3'
+import Project4 from './Project4'
 
 const Folio = (props) => {
     return (
@@ -8,6 +10,8 @@ const Folio = (props) => {
             <h1>Portfolio</h1>
             <Project1 />
             <Project2 />
+            <Project3 />
+            <Project4 />
         </div>
     )
 }
---
+++ src/components/Project4.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Project4 = (props) => {
+    return (
+        
+        <div class="container" id="projects">
+            <div class="row">
+                <div></div>
+                    <img class="img-fluid" id="pIMG" src="/images/project4.png" alt="Project1" />
+                </div>
+                <div id="intro">
+                    <p class="text-start" id="pnotes">- project two</p>
+                    <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                </div>
+        </div>
+    )
+}
+
+export default Project4
