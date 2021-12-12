3f786d785c7a70f4d7ff066e699d1411d262401e
vernellmiller <nezeey@gmail.com>
2021-12-11 19:37:15 -0500

    wip: adds resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,21 @@
 import React from "react";
+import "../index.css";
+import { Container, Row, Image, Col } from "react-bootstrap";
 
 const Resume = (props) => {
     return(
-        <h1>This is where my resume goes.</h1>
+        <>
+        <Container>
+            <Row>
+                <Col>
+                    <Image id="resume" src="/plain-01-01.png" alt="resume page 1" width="700px"/>
+                </Col>
+                <Col>
+                    <Image id="resume" src="/plain-01-02.png" alt="resume page 1" width="700px"/>
+                </Col>
+            </Row>
+        </Container>
+        </>
     );
 };
 
