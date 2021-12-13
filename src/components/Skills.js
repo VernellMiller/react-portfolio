0bbcc620c33e66ac3253c2ddd20f1a4987a68462
vernellmiller <nezeey@gmail.com>
2021-12-13 18:05:12 -0500

    refactors code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Skills = (props) => {
     return (
-        <div class="container bg-light">
+        <div class="bg-light">
             <p>- My Skills</p>
             <h1>Why Hire Me For your Next Project?</h1>
             <p>explanation of my qualifications</p>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,96 +3,97 @@ import React from 'react'
 const Projects = () => {
     return (
         <div id="projects" className="">
-            <div>
-                Dev Projects
-            </div>
-
             <div class="container">
+                <div id="portfolio" >
+                    Dev Projects
+                </div>
+
                 <div class="row">
-                    <div class="col-md-6" id="intro">
+                    <div id="intro" class="">
                         <p class="text-start" id="pnotes">- project one</p>
                         <h5 class="text-start" id="pnotes">CHOOSE YOUR POKEMON.</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                        </div>
+                        <div class="">
+                            <img class="" id="pIMG" src="/images/project1.png" alt="Project1" />
+                        </div>
+                </div>
+                
+                <div class="row">
+                    <div>
+                        <img class="" id="pIMG" src="/images/project2.png" alt="Project1" />
                     </div>
-                    <div class="col-md-1"></div>
-                        <img class="img-fluid" id="pIMG" src="/images/project1.png" alt="Project1" />
+                    <div id="intro">
+                        <p class="text-start" id="pnotes">- project two</p>
+                        <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                        <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
                 </div>
-            </div>
-            <div class="container" id="projects">
-            <div class="row">
-                <div></div>
-                    <img class="img-fluid" id="pIMG" src="/images/project2.png" alt="Project1" />
-                </div>
-                <div id="intro">
-                    <p class="text-start" id="pnotes">- project two</p>
-                    <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
-                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
-                </div>
-            </div>
-            <div class="container" id="projects">
-            <div class="row">
-                <div class="col-md-6" id="intro">
-                    <p class="text-start" id="pnotes">- project three</p>
-                    <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
-                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
-                </div>
-                <div class="col-md-1"></div>
-                    <img class="img-fluid rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
-            </div>
-            </div>
-            <div class="container" id="projects">
-            <div class="row">
-                <div></div>
-                    <img class="img-fluid shadow-lg p-3 mb-5 bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
-                </div>
-                <div id="intro">
-                    <p class="text-start" id="pnotes">- project two</p>
-                    <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
-                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
-                </div>
-            </div>
-
-            <div>
-                Design Projects
-            </div>
-
-            <div class="container" id="projects">
-            <div class="row">
-                <div class="col-md-6" id="intro">
-                    <p class="text-start" id="pnotes">- project three</p>
-                    <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
-                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
-                </div>
-                <div class="col-md-1"></div>
-                    <img class="img-fluid rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
-            </div>
-            </div>
-            <div class="container" id="projects">
-            <div class="row">
-                <div></div>
-                    <img class="img-fluid" id="pIMG" src="/images/project6.png" alt="Project1" />
-                </div>
-                <div id="intro">
-                    <p class="text-start" id="pnotes">- project six</p>
-                    <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
-                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
-                </div>
-            </div>
-            <div class="container" id="projects">
-            <div class="row">
-                <div class="col-md-6" id="intro">
-                    <p class="text-start" id="pnotes">- project three</p>
-                    <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
-                    <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
-                </div>
-                <div class="col-md-1">
-                    <img class="img-fluid" id="pIMG" src="/images/project7.png" alt="Project7" />
-                </div>
-            </div>
-            </div>
-
-
             
+                
+                <div class="row">
+                    <div id="intro" class="">
+                        <p class="text-start" id="pnotes">- project three</p>
+                        <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
+                        <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
+                    <div class="">
+                        <img class="img-fluid rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div>
+                        <img class="shadow-lg p-3 mb-5 bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
+                    </div>
+                    <div id="intro">
+                        <p class="text-start" id="pnotes">- project two</p>
+                        <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                        <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
+                </div>
+                
+
+                <div>
+                    Design Projects
+                </div>
+
+                
+                <div class="row">
+                    <div class="" id="intro">
+                        <p class="text-start" id="pnotes">- project three</p>
+                        <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
+                        <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
+                    <div class="">
+                        <img class="img-fluid rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div>
+                        <img class="img-fluid" id="pIMG" src="/images/project6.png" alt="Project1" />
+                    </div>
+                    <div id="intro">
+                        <p class="text-start" id="pnotes">- project six</p>
+                        <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                        <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div id="intro" class="">
+                        <p class="text-start" id="pnotes">- project three</p>
+                        <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
+                        <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
+                    <div class="">
+                        <img class="img-fluid" id="pIMG" src="/images/project7.png" alt="Project7" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
