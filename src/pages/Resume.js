bd0ff8c45fb962a3d36f0ebe9fff11ab39076510
vernellmiller <nezeey@gmail.com>
2021-12-13 18:04:43 -0500

    refactors code

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,54 +1,23 @@
-import React, { Component, useState, Render, onDocumentLoadSuccess } from "react";
+import React from "react";
 import "../index.css";
 import { Container, Row, Image, Col } from "react-bootstrap";
-import { Document, Page } from "react-pdf";
 
-const Resume = (props) => {
-    // state = {
-    //     numPages: null,
-    //     pageNumber: 1,
-    //   };
-    
-    //   onDocumentLoadSuccess = ({ numPages }) => {
-    //     this.setState({ numPages });
-    //   };
-
-    //   render() {
-    //     const { pageNumber, numPages } = this.state;
-
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-      }
-    
-    
-        return(
-            <>
-                {/* <Container>
-                    <Row>
-                        <Col>
-                            <Image id="resume" src="/plain-01-01.png" alt="resume page 1" width="700px"/>
-                        </Col>
-                        <Col>
-                            <Image id="resume" src="/plain-01-02.png" alt="resume page 1" width="700px"/>
-                        </Col>
-                    </Row>
-                </Container> */}
-
-                <div>
-                    <Document
-                        file="resume.pdf"
-                        onLoadSuccess={onDocumentLoadSuccess}
-                        >
-                        <Page pageNumber={pageNumber} />
-                    </Document>
-                    <p>Page {pageNumber} of {numPages}</p>
-                </div>
-            </>
-        );
-    // };
+const Resume = (props) => {    
+    return(
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <Image id="resume" src="/plain-01-01.png" alt="resume page 1" width="700px"/>
+                    </Col>
+                    <Col>
+                        <Image id="resume" src="/plain-01-02.png" alt="resume page 1" width="700px"/>
+                    </Col>
+                </Row>
+                    <p>Click to download</p>
+            </Container>
+        </>
+    );
 };
 
 export default Resume;
