527f6cd7b1bc1254e3816c6d310139602620fc0d
vernellmiller <nezeey@gmail.com>
2021-12-13 17:58:15 -0500

    changes links to anchor tags and refactors code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/esm/Button";
 import { Nav, Navbar, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css"
-
-// import { Nav, Navbar, Container } from "bootstrap";
 
 const Navigation = (props) => {
     return (
@@ -14,37 +11,22 @@ const Navigation = (props) => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Link class="text-decoration-none" id="link" to="/">Home</Link>
-                        <Link class="text-decoration-none" id="link" to="portfolio">Portfolio</Link>
+                        <a class="text-decoration-none" id="link" href="#Home">Home</a>
+                        <a class="text-decoration-none" id="link" href="#portfolio">Portfolio</a>
                         <Link class="text-decoration-none" id="link" to="resume">Resume</Link>
                     </Nav>
                     <Nav>
-                        <Button id="contact">Contact Me</Button>
+                        <a
+                            className="text-decoration-none" href="mailto:vernellmiller1@gmailcom">
+
+                            <div id="contact" className="rounded">
+                            Contact Me
+                            </div>
+                        </a>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-
-        // <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        //     <div class="container-fluid">
-        //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        //         <span class="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div class="collapse navbar-collapse" id="navbarNav">
-        //             <ul class="navbar-nav">
-        //                 <li class="nav-item">
-        //                     <Link class="text-decoration-none" id="link" to="/">Home</Link>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <Link class="text-decoration-none" id="link" to="portfolio">Portfolio</Link>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <Link class="text-decoration-none" id="link" to="resume">Resume</Link>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
     );
 };
 
