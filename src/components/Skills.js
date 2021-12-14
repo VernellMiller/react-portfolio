89e08135205d7973d186776213262bf0f20c1be4
vernellmiller <nezeey@gmail.com>
2021-12-14 16:39:46 -0500

    refactoring code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Skills = (props) => {
     return (
         <div class="container">
-            <p>- My Skills</p>
+            <p>My Skills</p>
             <p>HTML, CSS, JavaScript, REACT, Python, Django, jQuery, Bootstap,Mongoose, Express, REST APIs, MongoDB, Postgress, Node.js Front End 
             Development, Git, GitHub, Hereoku, Netligy, Back End Development, Full-Stack Development, Illustrator, Photoshop, Premiere Pro, 
             After Effects, Blender, Unreal Engine, Unity, InDesign, Substance Painter, Substance Designer, Office Research, 
