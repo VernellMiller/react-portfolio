bb922335fc21d6a7563065608e853afe8d89ea51
vernellmiller <nezeey@gmail.com>
2021-12-13 22:30:44 -0500

    adds onclick for nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/esm/Button";
 import { Nav, Navbar, Container } from "react-bootstrap";
 
 const Navigation = (props) => {
+
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar className="shadow-sm p-3 mb-5 bg-body rounded fixed-top" collapseOnSelect expand="md">
+        <Navbar className="shadow-sm p-3 mb-5 bg-body rounded fixed-top" collapseOnSelect expand="md" expanded={expanded}>
             <Container>
                 <Navbar.Brand>Vernell</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <a class="text-decoration-none" id="link" href="#Home">Home</a>
-                        <a class="text-decoration-none" id="link" href="#portfolio">Portfolio</a>
-                        <a class="text-decoration-none" id="link" href="#resume">Resume</a>
+                        <a class="text-decoration-none" id="link" href="#Home" onClick={() => setExpanded(false)}>Home</a>
+                        <a class="text-decoration-none" id="link" href="#portfolio" onClick={() => setExpanded(false)}>Portfolio</a>
+                        <a class="text-decoration-none" id="link" href="#resume" onClick={() => setExpanded(false)}>Resume</a>
                     </Nav>
                     <Nav>
                         <a
