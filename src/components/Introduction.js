6b3a0fe251f209e4e89dcccc45f773d90473ccd8
vernellmiller <nezeey@gmail.com>
2021-12-14 16:40:15 -0500

    adds bold font

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,12 +4,12 @@ const Introduction = (props) => {
             <div id="Home" class="container">
                 <div class="row gap100">
                     <div class="col-md-6 text-start" id="intro">
-                        <p>- introducing</p>
-                        <h3 id="heading">Hello</h3>
-                        <h1 id="heading">I'm Vernell</h1>
-                        <h1 id="heading" class="fs-1">Miller</h1>
+                        <h3 id="heading" class="fw-bold ">Hello</h3>
+                        <h1 id="heading" class="fw-bold ">I'm Vernell</h1>
+                        <h1 id="heading" class="fw-bold ">Miller</h1>
                         <p>An Atlanta-based Full Stack Engineer | 
-                        2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>“Individually, we are one drop. Together, we are an ocean.”
+                        2D/3D <br></br>Graphic Designer with a background in<br></br>Game Development and Design. I am an <br></br>exceptional team player, and I strongly believe <br></br>in the words of Japanese Author - Ryunosuke Satoro, <br></br>
+                        <p class="fw-bold ">“Individually, we are one drop. Together, we are an ocean.”</p>
                         </p>
                         <p>I have a passion for exploring new learning opportunities <br></br>and applying the new knowledge in exciting and creative ways.
                         </p>
