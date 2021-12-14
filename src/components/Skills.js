94bfe51c73a039ef8ce42f22ab395b4a36ee0cb0
vernellmiller <nezeey@gmail.com>
2021-12-13 22:30:04 -0500

    wip: adds additional skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Skills = (props) => {
     return (
-        <div class="bg-light">
+        <div class="container">
             <p>- My Skills</p>
+            <p>HTML, CSS, JavaScript, REACT, Python, Django, jQuery, Bootstap,Mongoose, Express, REST APIs, MongoDB, Postgress, Node.js Front End 
+            Development, Git, GitHub, Hereoku, Netligy, Back End Development, Full-Stack Development, Illustrator, Photoshop, Premiere Pro, 
+            After Effects, Blender, Unreal Engine, Unity, InDesign, Substance Painter, Substance Designer, Office Research, 
+            Building and Managing Client Relationships, Branding.</p>
             <h1>Why Hire Me For your Next Project?</h1>
             <p>explanation of my qualifications</p>
         </div>
