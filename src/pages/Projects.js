332e6ee2bf732ada6a41b23cf6f883e53cd1f0b8
vernellmiller <nezeey@gmail.com>
2021-12-13 20:57:40 -0500

    wip: fixes positioning and refactors code

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,95 +2,89 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div id="projects" className="">
-            <div class="container">
-                <div id="portfolio" >
-                    Dev Projects
+        <div id="projects" className="container">
+            <div class="container row gap100">
+                <div id="portfolio" class="text-start">
+                    <h1>Development Projects</h1>
                 </div>
-
-                <div class="row">
-                    <div id="intro" class="">
+{/* project1 */}
+                <div class="row gap100">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project one</p>
-                        <h5 class="text-start" id="pnotes">CHOOSE YOUR POKEMON.</h5>
+                        <h5 class="text-start" id="ptitle">CHOOSE YOUR POKEMON.</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
+                    </div>
+                        <div class="col-md-6">
+                            <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project1.png" alt="Project1" />
                         </div>
-                        <div class="">
-                            <img class="" id="pIMG" src="/images/project1.png" alt="Project1" />
-                        </div>
                 </div>
-                
-                <div class="row">
-                    <div>
-                        <img class="" id="pIMG" src="/images/project2.png" alt="Project1" />
+{/* project2 */}
+                <div class="row gap100">
+                    <div class="col-md-6">
+                        <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project2.png" alt="Project2" />
                     </div>
-                    <div id="intro">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project two</p>
-                        <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                        <h5 class="text-start" id="ptitle">LEVEL UP YOUR LINEUP</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
                     </div>
                 </div>
-            
-                
-                <div class="row">
-                    <div id="intro" class="">
+{/* project3 */}
+                <div class="row gap100">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project three</p>
-                        <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
+                        <h5 class="text-start" id="ptitle">BUILD YOUR TEAM.</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
                     </div>
-                    <div class="">
-                        <img class="img-fluid rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
+                    <div class="col-md-6">
+                        <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
                     </div>
                 </div>
-                
-                
-                <div class="row">
-                    <div>
-                        <img class="shadow-lg p-3 mb-5 bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
+{/* project4 */}
+                <div class="row gap100">
+                    <div class="col-md-6">
+                        <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
                     </div>
-                    <div id="intro">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project two</p>
-                        <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                        <h5 class="text-start" id="ptitle">LEVEL UP YOUR LINEUP</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
                     </div>
                 </div>
-                
-
-                <div>
-                    Design Projects
+{/* project5 */}
+                <div id="portfolio" class="text-start">
+                    <h1>Design Projects</h1>
                 </div>
-
-                
-                <div class="row">
-                    <div class="" id="intro">
+                <div class="row gap100">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project three</p>
-                        <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
+                        <h5 class="text-start" id="ptitle">BUILD YOUR TEAM.</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
                     </div>
-                    <div class="">
-                        <img class="img-fluid rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
+                    <div class="col-md-6">
+                        <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
                     </div>
                 </div>
-                
-                
+{/* project6 */}
                 <div class="row">
-                    <div>
-                        <img class="img-fluid" id="pIMG" src="/images/project6.png" alt="Project1" />
+                    <div class="col-md-6">
+                        <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project6.png" alt="Project1" />
                     </div>
-                    <div id="intro">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project six</p>
-                        <h5 class="text-start" id="pnotes">LEVEL UP YOUR LINEUP</h5>
+                        <h5 class="text-start" id="ptitle">LEVEL UP YOUR LINEUP</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
                     </div>
                 </div>
-                
-                <div class="row">
-                    <div id="intro" class="">
+{/* project7 */}
+                <div class="row gap100">
+                    <div id="intro" class="col-md-6">
                         <p class="text-start" id="pnotes">- project three</p>
-                        <h5 class="text-start" id="pnotes">BUILD YOUR TEAM.</h5>
+                        <h5 class="text-start" id="ptitle">BUILD YOUR TEAM.</h5>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer <br></br>fantasies while testing your Pokemon knowledge. <br></br> Are you the Very Best Pokemon Trainer? </p>
                     </div>
-                    <div class="">
-                        <img class="img-fluid" id="pIMG" src="/images/project7.png" alt="Project7" />
+                    <div class="col-md-6">
+                        <img class="shadow-sm p-3 mb-5 bg-body rounded bg-body rounded" id="pIMG" src="/images/project7.png" alt="Project7" />
                     </div>
                 </div>
             </div>
