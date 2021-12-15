4df4ddefe5cbcba1529a9454c903b85a6d47f79e
vernellmiller <nezeey@gmail.com>
2021-12-15 12:22:27 -0500

    refactors code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ const Skills = (props) => {
             <p>My Skills</p>
             <p>HTML, CSS, JavaScript, React, Python, Django, jQuery, Bootstrap, Mongoose, Express, REST APIs, MongoDB, Postgress, Node.js, Front End Development, Back End Development, Full-Stack Development, Hereoku, Netlify, Git, GitHub, Branding, Illustrator, Photoshop, Premiere Pro, After Effects, InDesign, Blender, Unreal Engine, 
             Unity, Substance Painter, Substance Designer, Office Research, Building and Managing Client Relationships.</p>
-            <h1 class="text-start">Take a look at my Project?</h1>
+            <h1 class="text-start">Take a look at my Project</h1>
         </div>
     )
 }
