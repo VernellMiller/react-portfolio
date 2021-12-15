2f607b35a0524335ac9a143ffb90bc990529f9d5
vernellmiller <nezeey@gmail.com>
2021-12-15 11:52:20 -0500

    edits skills list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,12 +4,9 @@ const Skills = (props) => {
     return (
         <div class="container">
             <p>My Skills</p>
-            <p>HTML, CSS, JavaScript, REACT, Python, Django, jQuery, Bootstap,Mongoose, Express, REST APIs, MongoDB, Postgress, Node.js Front End 
-            Development, Git, GitHub, Hereoku, Netligy, Back End Development, Full-Stack Development, Illustrator, Photoshop, Premiere Pro, 
-            After Effects, Blender, Unreal Engine, Unity, InDesign, Substance Painter, Substance Designer, Office Research, 
-            Building and Managing Client Relationships, Branding.</p>
-            <h1>Why Hire Me For your Next Project?</h1>
-            <p>explanation of my qualifications</p>
+            <p>HTML, CSS, JavaScript, React, Python, Django, jQuery, Bootstrap, Mongoose, Express, REST APIs, MongoDB, Postgress, Node.js, Front End Development, Back End Development, Full-Stack Development, Hereoku, Netlify, Git, GitHub, Branding, Illustrator, Photoshop, Premiere Pro, After Effects, InDesign, Blender, Unreal Engine, 
+            Unity, Substance Painter, Substance Designer, Office Research, Building and Managing Client Relationships.</p>
+            <h1 class="text-start">Take a look at my Project?</h1>
         </div>
     )
 }
