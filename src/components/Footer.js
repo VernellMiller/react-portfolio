386980c1936ec1dffeec9fcc77072b11a55d5ac5
vernellmiller <nezeey@gmail.com>
2021-12-15 12:29:20 -0500

    adds footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,15 @@ import React from 'react'
 const Footer = (props) => {
     return (
         <footer class="bg-light">
-            <p>GitHub</p>
-            <p>LinkedIn</p>
-            <p>Behance</p>
+            <a class="text-decoration-none" href="https://github.com/VernellMiller" target="_blank">
+                <p>GitHub</p>
+            </a>
+            <a class="text-decoration-none" href="https://www.linkedin.com/feed/" target="_blank">
+                <p>LinkedIn</p>
+            </a>
+            <a class="text-decoration-none" href="https://www.behance.net/VernellMiller" target="_blank">
+                <p>Behance</p>
+            </a>
         </footer>
     )
 }
