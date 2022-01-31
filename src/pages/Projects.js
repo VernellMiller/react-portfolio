82d4658d794389c1060d9a2773bd7fefbdb33229
vernellmiller <nezeey@gmail.com>
2022-01-31 17:24:54 -0500

    wip: reorders projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,27 +15,26 @@ const Projects = () => {
                             <h5 class="text-start" id="ptitle">CHOOSE YOUR POKEMON.</h5>
                         </a>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer fantasies while testing your Pokemon knowledge. Are you the Very Best Pokemon Trainer? <a class="text-decoration-none" href="https://github.com/VernellMiller/My-Top-Five" target="_blank" rel="noreferrer"> Github repo </a></p>
-                        
                     </div>
-                        <div class="col-lg-7 col-md-6">
-                            <a href="https://vernellmiller.github.io/My-Top-Five/" target="_blank" rel="noreferrer">
-                                <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project1.png" alt="Project1" />
-                            </a>
-                        </div>
+                    <div class="col-lg-7 col-md-6">
+                        <a href="https://vernellmiller.github.io/My-Top-Five/" target="_blank" rel="noreferrer">
+                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project1.png" alt="Project1" />
+                        </a>
+                    </div>
                 </div>
 {/* project2 */}
                 <div class="row flex-row align-items-center gap100">
-                    <div class="col-lg-7 col-md-6">
-                    <a href="https://quiet-scrubland-19641.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project2.png" alt="Project2" />
-                    </a>
-                    </div>
                     <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">project two</p>
                         <a class="text-decoration-none" href="https://quiet-scrubland-19641.herokuapp.com/" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">LEVEL UP YOUR LINEUP</h5>
                         </a>
                         <p class="text-start" id="pnotes">KEEPME allows me or YOU to schedule me in your lineup. That way we can kEEP track of what games I can play. Just enter a little bit of info on your team, time and place and we're ready to play. <a class="text-decoration-none" href="https://github.com/VernellMiller/keepme" target="_blank" rel="noreferrer"> Github repo </a></p>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <a href="https://quiet-scrubland-19641.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project2.png" alt="Project2" />
+                        </a>
                     </div>
                 </div>
 {/* project3 */}
@@ -55,17 +54,17 @@ const Projects = () => {
                 </div>
 {/* project4 */}
                 <div class="row gap100">
-                    <div class="col-lg-7 col-md-6">
-                    <a href="https://radiant-chamber-29667.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
-                    </a>
-                    </div>
                     <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">project four</p>
                         <a class="text-decoration-none" href="https://radiant-chamber-29667.herokuapp.com/" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">RAAM - A FOOD PANTRY APP</h5>
                         </a>
                         <p class="text-start" id="pnotes">Community based, all-in-one app where anyone can create a local food pantry to lend a helping hand to their community. Also, locate a local pantry to donate or pick up essential items. <a class="text-decoration-none" href="https://github.com/mariorojasac/raam" target="_blank" rel="noreferrer"> Github repo </a></p>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <a href="https://radiant-chamber-29667.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
+                        </a>
                     </div>
                 </div>
 {/* project5 */}
@@ -88,13 +87,13 @@ const Projects = () => {
                 </div>
 {/* project6 */}
                 <div class="row">
-                    <div class="col-lg-7 col-md-6">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project6.png" alt="Project1" />
-                    </div>
                     <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">branding, design and learning</p>
                         <h5 class="text-start" id="ptitle">LOGO DESIGN</h5>
                         <p class="text-start" id="pnotes">One of many logo designs I've created. This was a personal project where I learned more about design.</p>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project6.png" alt="Project1" />
                     </div>
                 </div>
 {/* project7 */}
