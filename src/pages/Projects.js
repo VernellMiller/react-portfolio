acc25c41ec21082472fcd62f60089e64bedebf28
vernellmiller <nezeey@gmail.com>
2022-01-31 17:35:27 -0500

    fix: adds responsivness to project images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,9 +16,9 @@ const Projects = () => {
                         </a>
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer fantasies while testing your Pokemon knowledge. Are you the Very Best Pokemon Trainer? <a class="text-decoration-none" href="https://github.com/VernellMiller/My-Top-Five" target="_blank" rel="noreferrer"> Github repo </a></p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <a href="https://vernellmiller.github.io/My-Top-Five/" target="_blank" rel="noreferrer">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project1.png" alt="Project1" />
+                        <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project1.png" alt="Project1" />
                         </a>
                     </div>
                 </div>
@@ -31,9 +31,9 @@ const Projects = () => {
                         </a>
                         <p class="text-start" id="pnotes">KEEPME allows me or YOU to schedule me in your lineup. That way we can kEEP track of what games I can play. Just enter a little bit of info on your team, time and place and we're ready to play. <a class="text-decoration-none" href="https://github.com/VernellMiller/keepme" target="_blank" rel="noreferrer"> Github repo </a></p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <a href="https://quiet-scrubland-19641.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project2.png" alt="Project2" />
+                        <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project2.png" alt="Project2" />
                         </a>
                     </div>
                 </div>
@@ -46,9 +46,9 @@ const Projects = () => {
                         </a>
                         <p class="text-start" id="pnotes">Calling all sports Fanatics. Your time has come. Use your knowledge of the game, create your players and build your favorite teams.<a class="text-decoration-none" href="https://github.com/VernellMiller/sports-chat-frontend" target="_blank" rel="noreferrer"> Github repo </a></p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <a class="text-decoration-none" href="https://sports-team-builder.netlify.app/" target="_blank" rel="noreferrer">
-                            <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
+                            <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
                         </a>
                     </div>
                 </div>
@@ -61,9 +61,9 @@ const Projects = () => {
                         </a>
                         <p class="text-start" id="pnotes">Community based, all-in-one app where anyone can create a local food pantry to lend a helping hand to their community. Also, locate a local pantry to donate or pick up essential items. <a class="text-decoration-none" href="https://github.com/mariorojasac/raam" target="_blank" rel="noreferrer"> Github repo </a></p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <a href="https://radiant-chamber-29667.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
+                        <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
                         </a>
                     </div>
                 </div>
@@ -79,9 +79,9 @@ const Projects = () => {
                         </a>
                         <p class="text-start" id="pnotes">An app where I designed all aspects of the project. This was a personal project where taught myself various design techniques.</p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <a href="https://www.behance.net/gallery/121996489/TRAFFIC" target="_blank" rel="noreferrer">
-                            <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
+                            <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
                         </a>
                     </div>
                 </div>
@@ -92,8 +92,8 @@ const Projects = () => {
                         <h5 class="text-start" id="ptitle">LOGO DESIGN</h5>
                         <p class="text-start" id="pnotes">One of many logo designs I've created. This was a personal project where I learned more about design.</p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
-                        <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project6.png" alt="Project1" />
+                    <div class="col-lg-7 col-md-6 col-sm-12">
+                        <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project6.png" alt="Project1" />
                     </div>
                 </div>
 {/* project7 */}
@@ -105,9 +105,9 @@ const Projects = () => {
                             </a>
                         <p class="text-start" id="pnotes">Learning and practicing in my favorite vector base software, Adobe Illustrator. I was inspired by various 3d renders.</p>
                     </div>
-                    <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <a class="text-decoration-none" href="https://www.behance.net/gallery/112376301/Illustrator-Practice" target="_blank" rel="noreferrer">
-                            <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project7.png" alt="Project7" />
+                            <img class="img-fluid shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project7.png" alt="Project7" />
                         </a>
                     </div>
                 </div>
