0db2dc41b158de01b8438b877f78609e2d47c1af
vernellmiller <nezeey@gmail.com>
2022-01-31 17:03:31 -0500

    wip: adds additional responsivness

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
                 </div>
 {/* project1 */}
                 <div class="row flex-row align-items-center gap100">
-                    <div id="decripton" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">project one</p>
                         <a class="text-decoration-none" href="https://vernellmiller.github.io/My-Top-Five/" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">CHOOSE YOUR POKEMON.</h5>
@@ -17,7 +17,7 @@ const Projects = () => {
                         <p class="text-start" id="pnotes">Allows you to live out your pokemon trainer fantasies while testing your Pokemon knowledge. Are you the Very Best Pokemon Trainer? <a class="text-decoration-none" href="https://github.com/VernellMiller/My-Top-Five" target="_blank" rel="noreferrer"> Github repo </a></p>
                         
                     </div>
-                        <div class="col-md-6">
+                        <div class="col-lg-7 col-md-6">
                             <a href="https://vernellmiller.github.io/My-Top-Five/" target="_blank" rel="noreferrer">
                                 <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project1.png" alt="Project1" />
                             </a>
@@ -25,12 +25,12 @@ const Projects = () => {
                 </div>
 {/* project2 */}
                 <div class="row flex-row align-items-center gap100">
-                    <div class="col-md-6">
+                    <div class="col-lg-7 col-md-6">
                     <a href="https://quiet-scrubland-19641.herokuapp.com/" target="_blank" rel="noreferrer">
                         <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project2.png" alt="Project2" />
                     </a>
                     </div>
-                    <div id="description" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">project two</p>
                         <a class="text-decoration-none" href="https://quiet-scrubland-19641.herokuapp.com/" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">LEVEL UP YOUR LINEUP</h5>
@@ -40,14 +40,14 @@ const Projects = () => {
                 </div>
 {/* project3 */}
                 <div class="row gap100">
-                    <div id="description" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">project three</p>
                         <a class="text-decoration-none" href="https://sports-team-builder.netlify.app/" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">BUILD YOUR TEAM.</h5>
                         </a>
                         <p class="text-start" id="pnotes">Calling all sports Fanatics. Your time has come. Use your knowledge of the game, create your players and build your favorite teams.<a class="text-decoration-none" href="https://github.com/VernellMiller/sports-chat-frontend" target="_blank" rel="noreferrer"> Github repo </a></p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-lg-7 col-md-6">
                         <a class="text-decoration-none" href="https://sports-team-builder.netlify.app/" target="_blank" rel="noreferrer">
                             <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project3.png" alt="Project3" />
                         </a>
@@ -55,12 +55,12 @@ const Projects = () => {
                 </div>
 {/* project4 */}
                 <div class="row gap100">
-                    <div class="col-md-6">
+                    <div class="col-lg-7 col-md-6">
                     <a href="https://radiant-chamber-29667.herokuapp.com/" target="_blank" rel="noreferrer">
                         <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project4.png" alt="Project1" />
                     </a>
                     </div>
-                    <div id="description" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">project four</p>
                         <a class="text-decoration-none" href="https://radiant-chamber-29667.herokuapp.com/" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">RAAM - A FOOD PANTRY APP</h5>
@@ -73,14 +73,14 @@ const Projects = () => {
                     <h1 class="fw-bold ">Design Projects</h1>
                 </div>
                 <div class="row gap100">
-                    <div id="description" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">branding, design and learning</p>
                         <a class="text-decoration-none" href="https://www.behance.net/gallery/121996489/TRAFFIC" target="_blank" rel="noreferrer">
                             <h5 class="text-start" id="ptitle">TRAFFIC</h5>
                         </a>
                         <p class="text-start" id="pnotes">An app where I designed all aspects of the project. This was a personal project where taught myself various design techniques.</p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-lg-7 col-md-6">
                         <a href="https://www.behance.net/gallery/121996489/TRAFFIC" target="_blank" rel="noreferrer">
                             <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project5.png" alt="Project5" />
                         </a>
@@ -88,10 +88,10 @@ const Projects = () => {
                 </div>
 {/* project6 */}
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-lg-7 col-md-6">
                         <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project6.png" alt="Project1" />
                     </div>
-                    <div id="description" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">branding, design and learning</p>
                         <h5 class="text-start" id="ptitle">LOGO DESIGN</h5>
                         <p class="text-start" id="pnotes">One of many logo designs I've created. This was a personal project where I learned more about design.</p>
@@ -99,14 +99,14 @@ const Projects = () => {
                 </div>
 {/* project7 */}
                 <div class="row gap100">
-                    <div id="description" class="col-md-6">
+                    <div id="description" class="col-lg-5 col-md-6 col-sm-12">
                         <p class="text-start" id="pnotes">hobby and learning</p>
                             <a class="text-decoration-none" href="https://www.behance.net/gallery/112376301/Illustrator-Practice" target="_blank" rel="noreferrer">
                                 <h5 class="text-start" id="ptitle">Illustrator Design</h5>
                             </a>
                         <p class="text-start" id="pnotes">Learning and practicing in my favorite vector base software, Adobe Illustrator. I was inspired by various 3d renders.</p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-lg-7 col-md-6">
                         <a class="text-decoration-none" href="https://www.behance.net/gallery/112376301/Illustrator-Practice" target="_blank" rel="noreferrer">
                             <img class="shadow-sm p-3 bg-body rounded bg-body rounded" id="pIMG" src="/images/project7.png" alt="Project7" />
                         </a>
